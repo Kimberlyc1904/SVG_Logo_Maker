782ca6b3f538288025d7bcfa188afc50b482979f
kimberlyc <kimberlyc1904@yahoo.com>
2023-06-12 16:10:03 -0700

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # SVG_Logo_Maker
 SVG_Logo_Maker
+
+## Weather_Dashboard_Homework6
+
+- My motivation in building a SVG Logo Maker is to get better at JavaScripting.
+- I built this SVG Logo Maker to allow someone to quickly create a logo using different shapes, text and colors.
+- I learned a little JavaScripting.
+
+## User Story
+```
+AS a freelance web developer
+I WANT to generate a simple logo for my projects
+SO THAT I don't have to pay a graphic designer
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+```
+
+## Usage
+
+To use the Weather Dashboard, type type in a city to the search and click the "get city" button. You will be presented with current weather and the next 5 days of weather forcast.
+
+https://github.com/Kimberlyc1904/SVG_Logo_Maker
+
+
+![screenshot](assets/images/ScreenShot.png)
+
+## Credits
+
+```
+Erwin Jocosing Student. @ github.com/ej619
+W3 Schools https://www.w3schools.com/
+ChatGPT https://chat.openai.com/
+```
--- library/cli.js
+++ library/cli.js
@@ -1,55 +1,67 @@
 const inquire = require('inquirer');
 const {writeFile} = require('fs/promises')
-// import SVG, circle , triangle, square
+const { Circle, Triangle, Square } = require('./shapes');
+const SVG = require('./svg');
+
 const questions = [
     {
-        type: 'input',
-        message: 'Enter up to 3 characters of text',
+        type:'input',
+        message: 'Enter 3 character text',
         name: 'text',
     },
     {
-        type: 'input',
-        message: 'Enter text color',
+        type:'input',
+        message:'Enter text color',
         name: 'color',
     },
     {
-        type: 'list',
-        mesaage: 'Select a shape',
-        name: 'shape',
-        choices: ['circle','triangle','square'],
+        type:'list',
+        message: 'Select shape',
+        name: 'shapeType',
+        choices: ['circle', 'triangle', 'square'],
     },
     {
-        type: 'input',
-        message: 'select a shape color',
-        name: 'shapeColor',
+        type:'input',
+        message: 'Enter Shape color',
+        name:'shapeColor',
     }
 ];
+
 class CLI {
     run(){
         return inquire.prompt(questions)
         .then((answers) =>
         createSVG(answers)
-        )  
+        )
     }
+}
+
+function createSVG (answers){
+const svg = new SVG();
+    console.log(answers.shapeType);
+    let shape;
+        switch (answers.shapeType) {
+          case "circle":
+             shape = new Circle(answers.shapeColor);
+            break;
+           case "square":
+             shape = new Square(answers.shapeColor);
+             break;
+           case "triangle":
+            shape = new Triangle(answers.shapeColor);
+            break;
+        }
+
+       svg.setShape(shape);
+       svg.setText(answers.text, answers.color)
+       const svgContent = svg.render();
+
+       writeFile('output.svg', svgContent)
+       .then(() => console.log('SVG file created successfully!'))
+       .catch((error) => console.error('Error writing SVG file:', error));
+        
 };
 
-function createSVG (answers) { 
-
-    let shape;
-            switch (answers.shapeType) {
-              case "circle":
-               shape = new Circle();
-               break;
-
-            case "square":
-               shape = new Square();
-               break;
-
-            case "triangle":
-               shape = new Triangle();
-               break;
-        }
-    };
 module.exports = CLI
 
         // shape.setColor(answers.shapeColor);
